cece29761cb37d35d608df78f5abe981bec12671
Samson Ayodele <samson.idowu@bankly.ng>
2022-09-30 05:42:56 +0100

    Directives

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelName = 'Hilton Hotel';
+
+  numberOfRooms = 10;
+
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air conditioner, Free wi-fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://unsplash.com/photos/51adhgg5KkE',
+      checkinTime: new Date('30-sept-2022'),
+      checkoutTime: new Date('01-oct-2022'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air conditioner, Free wi-fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos: 'https://unsplash.com/photos/51adhgg5KkE',
+      checkinTime: new Date('30-sept-2022'),
+      checkoutTime: new Date('01-oct-2022'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private suite',
+      amenities: 'Air conditioner, Free wi-fi, TV, Bathroom, Kitchen',
+      price: 1500,
+      photos: 'https://unsplash.com/photos/51adhgg5KkE',
+      checkinTime: new Date('30-sept-2022'),
+      checkoutTime: new Date('01-oct-2022'),
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+}
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,17 @@
+// import { NumberSymbol } from "@angular/common";
+
+export interface Room {
+  totalRooms: number;
+  availableRooms: number;
+  bookedRooms: number;
+}
+
+export interface RoomList {
+  roomNumber: number;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photos: string;
+  checkinTime: Date;
+  checkoutTime: Date;
+}
